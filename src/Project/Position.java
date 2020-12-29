9fb7e5e3ff93de3de4e4eba969ab17bb1f0ad36a
DamianDuy <gamergray8@gmail.com>
2020-12-29 14:33:17 +0100

    Implemented Position class

--- src/Project/Position.java
+++ src/Project/Position.java
@@ -1,4 +1,42 @@
 package Project;
 
 public class Position {
+    private int positionX;
+    private int positionY;
+    Position(int positionX, int positionY){
+        setPositionX(positionX);
+        setPositionY(positionY);
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(int positionX) {
+        if (positionX < 0){
+            throw new IllegalArgumentException("Negative X position.");
+        }
+        this.positionX = positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(int positionY) {
+        if (positionY < 0){
+            throw new IllegalArgumentException("Negative Y position.");
+        }
+        this.positionY = positionY;
+    }
+    public boolean equals(Position o){
+        if (o.getPositionX() == this.getPositionX() && o.getPositionY() == this.getPositionY()){
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public String toString(){
+        return String.format(positionX + ", " + positionY);
+    }
 }
---
+++ src/Project/PositionTest.java
@@ -0,0 +1,25 @@
+package Project;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PositionTest {
+    @Test
+    public void equalsTest(){
+        Position position1 = new Position(4,5);
+        Position position2 = new Position(4,5);
+        assertEquals(true, position1.equals(position2));
+    }
+    @Test
+    public void notEqualsTest(){
+        Position position1 = new Position(4,5);
+        Position position2 = new Position(5,5);
+        assertEquals(false, position1.equals(position2));
+    }
+    @Test
+    public void toStringTest(){
+        Position position1 = new Position(4,5);
+        assertEquals("4, 5", position1.toString());
+    }
+}
