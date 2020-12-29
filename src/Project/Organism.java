5245ccd2c1ab335598c463f6734fc5d00d24ae15
DamianDuy <gamergray8@gmail.com>
2020-12-29 15:50:18 +0100

    Added abstract methods to the Organism class

--- src/Project/Organism.java
+++ src/Project/Organism.java
@@ -1,4 +1,15 @@
 package Project;
 
-public class Organism {
+public abstract class Organism{
+    private int power;
+    private int initiative;
+    private Position position;
+    private int powerToReproduce;
+    private char sign;
+    private World world;
+    public abstract void move();
+    public abstract void action();
+    public abstract void initParams();
+    public abstract Organism clone();
+
 }
--- src/Project/World.java
+++ src/Project/World.java
@@ -1,4 +1,32 @@
 package Project;
 
+import java.util.ArrayList;
+
 public class World {
+    private int worldX;
+    private int worldY;
+    private int turn;
+    private ArrayList<Organism> organisms = new ArrayList<Organism>();
+    private ArrayList<Organism> newOrganisms = new ArrayList<Organism>();
+    private char separator = '.';
+    World(int worldX, int worldY){
+        setTurn();
+        setWorldX(worldX);
+        setWorldY(worldY);
+    }
+    public void setTurn(){
+        this.turn = 0;
+    }
+    public void setWorldX(int worldX){
+        if (worldX < 0){
+            throw new IllegalArgumentException("Negative X size.");
+        }
+        this.worldX = worldX;
+    }
+    public void setWorldY(int worldY){
+        if (worldY < 0){
+            throw new IllegalArgumentException("Negative Y size.");
+        }
+        this.worldY = worldY;
+    }
 }
